509768c5658cda86fcdb406711d05cf50e4bb174
Eithan Smith <yahia.smeism@sappro-bilisim.com>
2024-11-17 19:53:19 +0300

    add missing endpoints

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -2,11 +2,6 @@ const Todo = require('../models/Todo');
 const joi = require('joi');
 
 
-const todoScheme = joi.object({
-    title: joi.string().required(),
-    description: joi.string().required(),
-    completed: joi.boolean().default(false)
-});
 
 const getAllTodos = async (req, res) => {
     const page = parseInt(req.query.page) || 1;
@@ -32,10 +27,15 @@ const getAllTodos = async (req, res) => {
         res.status(500).json({ error: error.message });
     }
 };
+const createTodoScheme = joi.object({
+    title: joi.string().required(),
+    description: joi.string().required(),
+    completed: joi.boolean().default(false)
+});
 
 const createTodo = async (req, res) => {
     try {
-        const { error } = todoScheme.validate(req.body);
+        const { error } = createTodoScheme.validate(req.body);
         if (error) return res.status(400).json({ error: error.details[0].message });
 
         const todo = await Todo.create(req.body);
@@ -44,8 +44,38 @@ const createTodo = async (req, res) => {
         res.status(500).json({ error: error.message });
     }
 };
+const updateTodoScheme = joi.object({
+    title: joi.string(),
+    description: joi.string(),
+    completed: joi.boolean(),
+});
+const updateTodo = async (req, res) => {
+    const { id } = req.params;
+    const { title, description, completed } = req.body;
+    const { error } = updateTodoScheme.validate(req.body);
+    if (error) return res.status(400).json({ error: error.details[0].message });
+
+    try {
+        const todo = await Todo.findOneAndUpdate({ _id: id }, { title, description, completed }, { new: true });
+        res.status(200).json(todo);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
+
+const deleteTodo = async (req, res) => {
+    const { id } = req.params;
+    try {
+        await Todo.findOneAndDelete({ _id: id });
+        res.status(204);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
 
 module.exports = {
     getAllTodos,
-    createTodo
+    createTodo,
+    updateTodo,
+    deleteTodo,
 };
